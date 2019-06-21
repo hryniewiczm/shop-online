020d808db1e367b4ce9a081464e034d78df6f7ee
Magdah <magdalena.hryniewicz84@gmail.com>
2019-06-22 00:56:11 +0200

    Exporting orders to CSV files and adding custom actions to the administration site

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -19,11 +19,13 @@ class Order(models.Model):
         return 'Order {}'.format(self.id)
 
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.item.all())
+        return sum(item.get_cost() for item in self.items.all())
+
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, related_name='items',
-                                    on_delete=models.CASCADE)
+    order = models.ForeignKey(Order,
+                              related_name='items',
+                              on_delete=models.CASCADE)
     product = models.ForeignKey(Product,
                                 related_name='order_items',
                                 on_delete=models.CASCADE)
