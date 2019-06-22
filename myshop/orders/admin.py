afc3a10729a763f1a5155b230f079631cfdc0510
Magdah <magdalena.hryniewicz84@gmail.com>
2019-06-22 12:38:40 +0200

    rendering PDF files

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -31,6 +31,11 @@ def order_detail(obj):
     return mark_safe('<a href="{}">View</a>'.format(
         reverse('orders:admin_order_detail', args=[obj.id])))
 
+def order_pdf(obj):
+    return mark_safe('<a href="{}">PDF</a>'.format(
+        reverse('orders:admin_order_pdf', args=[obj.id])))
+order_pdf.short_description = 'Invoice'
+
 class OrderItemInLine(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -39,7 +44,8 @@ class OrderItemInLine(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 
                     'address', 'postal_code', 'city', 
-                    'paid', 'created', 'updated', order_detail]
+                    'paid', 'created', 'updated', order_detail,
+                    order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInLine]
     actions = [export_to_csv]
